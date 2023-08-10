9c5249f87ee22ceeaa7b2ddb1c7ef78bf10b9ab9
Federico <sivieri.federico@gmail.com>
2023-08-10 16:03:01 +0200

    add logic in create expenses

--- src/routers/cashIn/cashIn.model.ts
+++ src/routers/cashIn/cashIn.model.ts
@@ -1,15 +1,18 @@
 export class CashIn {
 	amount: number;
+	residue: number;
 	startDate: Date;
 	note: null | string;
 	endDate?: Date;
 	constructor(
 		amount: number,
+		residue: number,
 		startDate: Date,
 		note: null | string,
 		endDate?: Date
 	) {
 		this.amount = amount;
+		this.residue = residue;
 		this.startDate = startDate;
 		this.note = note;
 		this.endDate = endDate;
@@ -20,11 +23,12 @@ export class EntityCashIn extends CashIn {
     ID: string;
     constructor(
         amount: number,
+		residue: number,
         startDate: Date,
         note: null | string,
         id: string,
 		endDate?: Date) {
-        super(amount, startDate, note, endDate);
+        super(amount, residue, startDate, note, endDate);
         this.ID = id;
     }
 }
--- src/routers/expenses/expenses.service.ts
+++ src/routers/expenses/expenses.service.ts
@@ -1,11 +1,33 @@
 import axios from 'axios';
 import UTILS from '../../utils/utils';
-import { EntityExpenses } from './expenses.model';
+import * as serviceCashIn from '../cashIn/cashIn.service';
+import { EntityExpenses, Expenses } from './expenses.model';
+import { EntityCashIn } from '../cashIn/cashIn.model';
 
 const baseUrl = process.env.BASE_URL_DATABASE;
+const queryFilter = '?orderBy="cashInID"&equalTo=';
 
-export const create = async (bodyReq: object) => {
-    return await axios.post(`${baseUrl}expenses.json`, bodyReq);
+export const create = async (bodyReq: Expenses) => {
+    const expenses = await axios.post(`${baseUrl}expenses.json`, bodyReq);
+    const checkCashIn = await axios.get<EntityCashIn>(`${baseUrl}cashin/${bodyReq.cashInID}.json`);
+    if (checkCashIn.data.residue > 0) { // verifica che il bonifico indicato per la spesa abbia ancora budget
+        if (checkCashIn.data.residue - bodyReq.amount < 0) {
+            const partResidue = bodyReq.amount - checkCashIn.data.residue;
+            await serviceCashIn.updateByID(bodyReq.cashInID, { residue: 0 });
+            const cashInDate = checkCashIn.data.startDate;
+            cashInDate.setMonth(cashInDate.getMonth() + 1);
+            const newCashIn =
+                await axios.get<EntityCashIn>(`${baseUrl}cashin.json${queryFilter}"${cashInDate.toString()}"`);
+            const newResidue = newCashIn.data.residue - partResidue;
+            await serviceCashIn.updateByID(newCashIn.data.ID, {...newCashIn, amount: newResidue});
+        } else {
+            const residue = checkCashIn.data.residue - bodyReq.amount;
+            await serviceCashIn.updateByID(bodyReq.cashInID, { amount: residue });
+        }
+    } else {
+        console.log('ciao');
+    }
+    return expenses;
 };
 
 export const getAll = async (): Promise<EntityExpenses[]> => {
